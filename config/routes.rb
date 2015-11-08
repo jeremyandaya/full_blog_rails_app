2d8bcbf91583a9a6fb18868981a7a0ccd922930d
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-11-08 18:47:17 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/new'
+  get 'users/new'
+
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users
+end
